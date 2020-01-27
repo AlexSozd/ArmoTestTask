0f60a791c1a5038c2e9924935dde0e8729513e8b
AlexSozd <55886669+AlexSozd@users.noreply.github.com>
2020-01-27 15:29:34 +0300

    Add files via upload

---
+++ ArmoTestTasks_Sozdatelev/TestTask1/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace TestTask1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(519, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(260, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(260, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(260, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(27, 28);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(191, 276);
+            this.treeView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(257, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Текущий обрабатываемый файл: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(257, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(257, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Кол-во обработанных файлов: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(257, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Прошедшее время: ";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(260, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Остановить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(260, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Продолжить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(471, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(391, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 328);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Поиск файлов";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ArmoTestTasks_Sozdatelev/TestTask1/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestTask1
+{
+    public partial class Form1 : Form
+    {
+        string dir, reg_ex;
+        char[] syms;
+        List<string> dirlist = new List<string>();
+        List<TreeNode> trlist = new List<TreeNode>();
+        TimeSpan ts = new TimeSpan();
+        DateTime bt;
+        public Form1()
+        {
+            InitializeComponent();
+            string name = "userparameters.txt";
+            if(File.Exists(name))
+            {
+                using (StreamReader sr = new StreamReader(name))
+                {
+                    dir = sr.ReadLine();
+                    textBox1.Text = dir;
+                    reg_ex = sr.ReadLine();
+                    textBox2.Text = reg_ex;
+                    textBox3.Text = sr.ReadLine();
+                    syms = textBox3.Text.ToArray();
+                }
+            }
+            label2.Text = "";
+            label5.Text = "";
+            label6.Text = ts.Hours + ":" + ts.Minutes + ":" + ts.Seconds + "." + ts.Milliseconds;
+        }
+
+        //Two variants - timer with folders' list or recursion with threads
+        //Два варианта - таймер со списком папок или, возможно, рекурсивная функция с созданием пула (асинхронных) потоков
+
+        //Кнопка №1 - "Поиск"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dir = textBox1.Text;
+            reg_ex = "@" + textBox2.Text;
+            syms = textBox3.Text.ToArray();
+            if(Directory.Exists(dir))
+            {
+                if (treeView1.Nodes.Count > 0)
+                {
+                    treeView1.Nodes.Clear();
+                    
+                    ts = new TimeSpan();
+                    label6.Text = ts.Hours + ":" + ts.Minutes + ":" + ts.Seconds + "." + ts.Milliseconds;
+                }
+                //Begin search
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = false;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                label5.Text = "0";
+
+                bt = DateTime.Now;
+                //timer1.Start();
+                TreeNode tr = new TreeNode(dir);
+                tr.Name = dir;
+                treeView1.Nodes.Add(tr);
+                
+                dirlist.Add(dir);
+                trlist.Add(tr);
+                /*string[] files = Directory.GetFiles(dir, reg_ex, SearchOption.TopDirectoryOnly);
+                foreach (string s in files)
+                {
+                    tr.Nodes.Add(s);
+                    label2.Text = s;
+                    label5.Text = (int.Parse(label5.Text) + 1).ToString();
+                }
+                string[] subdirs = Directory.GetDirectories(dir);
+                foreach(string s in subdirs)
+                {
+                    //Search in subdirs
+                    //Rec_search(tr, s);
+                    dirlist.Add(s);
+                }*/
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Директория с таким именем не найдена");
+            }
+        }
+
+        //private void Rec_search(TreeNode tr, string dir)
+        private void Dir_search()
+        {
+            //tr.Nodes.Add(dir);
+            TreeNode tr = trlist[0], tr1;
+            trlist.RemoveAt(0);
+            string dir1 = dirlist[0];
+            dirlist.Remove(dir1);
+
+            label2.Text = dir1;
+            //string[] files = Directory.GetFiles(dir1, reg_ex, SearchOption.TopDirectoryOnly);
+            string[] files = Directory.GetFiles(dir1);
+            foreach (string s in files)
+            {
+                if (Regex.IsMatch(s.Substring(s.LastIndexOf('\\') + 1), reg_ex, RegexOptions.IgnoreCase))
+                {
+                    //tr.Nodes.Add(s);
+                    label2.Text = s;
+                    if (SymFinded(s))
+                    {
+                        tr.Nodes.Add(s.Substring(s.LastIndexOf('\\') + 1));
+                    }
+                    label5.Text = (int.Parse(label5.Text) + 1).ToString();
+                }
+            }
+            string[] subdirs = Directory.GetDirectories(dir1);
+            foreach (string s in subdirs)
+            {
+                //Rec_search(tr, s);
+                tr1 = new TreeNode(s.Substring(s.LastIndexOf('\\') + 1));
+                tr1.Name = s;
+                tr.Nodes.Add(tr1);
+                trlist.Add(tr1);
+                dirlist.Add(s);
+            }
+            tr.Expand();
+        }
+        //Кнопка №2 - "Остановить"
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (dirlist.Count > 0)
+            {
+                Dir_search();
+                ts = ts + (DateTime.Now - bt);
+                label6.Text = ts.Hours + ":" + ts.Minutes + ":" + ts.Seconds + "." + ts.Milliseconds;
+                bt = DateTime.Now;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                button1.Enabled = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                label2.Text = "";
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+            }
+        }
+        //Кнопка №3 - "Продолжить (поиск)"
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            bt = DateTime.Now;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+        }
+
+        private bool SymFinded(string fname)
+        {
+            bool res = false;
+            string buf;
+
+            //using (StreamReader sr = new StreamReader(fname))
+            using (FileStream sr = File.OpenRead(fname))
+            {
+                byte[] barr = new byte[sr.Length];
+                while (((sr.Read(barr, 0, barr.Length)) > 0) && !res)
+                {
+                    buf = System.Text.Encoding.Default.GetString(barr);
+                    for (int i = 0; i < syms.Length; i++)
+                    {
+                        if (buf.Contains(syms[i]))
+                        {
+                            res = true;
+                        }
+                    }
+                }
+                
+                /*while (((buf = sr.ReadLine()) != null) && !res)
+                {
+                    for (int i = 0; i < syms.Length; i++)
+                    {
+                        if (buf.Contains(syms[i]))
+                        {
+                            res = true;
+                        }
+                    }
+                }*/
+            }
+            
+            return res;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("userparameters.txt"))
+            {
+                sw.WriteLine(dir);
+                sw.WriteLine(reg_ex);
+                sw.Write(syms);
+            }
+        }
+    }
+}
